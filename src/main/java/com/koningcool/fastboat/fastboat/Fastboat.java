d1528fa65902f8cb91f5d4246a7c9dc2352d790f
koningcool <koningcool@outlook.com>
2022-06-25 20:48:25 +0200

    added event listener for the boat

--- src/main/java/com/koningcool/fastboat/fastboat/Fastboat.java
+++ src/main/java/com/koningcool/fastboat/fastboat/Fastboat.java
@@ -1,17 +1,34 @@
 package com.koningcool.fastboat.fastboat;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Fastboat extends JavaPlugin {
+public final class Fastboat extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        System.out.println(ChatColor.GREEN + "FastBoat is enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println(ChatColor.RED + "FastBoat is disabled!");
     }
+    @SuppressWarnings("deprecated")
+    @EventHandler
+    public void OnVehicleDrive(VehicleMoveEvent event){
+        Entity vehicle = event.getVehicle();
+        if (vehicle instanceof Boat){
+            Boat boat = (Boat) vehicle;
+            boat.setVelocity(boat.getVelocity().multiply(10.0D));
+            }
+        }
+
+
 }
