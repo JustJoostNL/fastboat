c7a11ac7fb2d9181b28a59e5a561ee059d5e7f91
koningcool <koningcool@outlook.com>
2022-06-25 22:25:29 +0200

    add onload message, add permission

--- src/main/java/com/koningcool/fastboat/fastboat/Fastboat.java
+++ src/main/java/com/koningcool/fastboat/fastboat/Fastboat.java
@@ -13,6 +13,12 @@ import org.bukkit.util.Vector;
 
 public final class Fastboat extends JavaPlugin implements Listener {
 
+
+    @Override
+    public void onLoad(){
+        System.out.println(ChatColor.GREEN + "FastBoat loaded!");
+    }
+
     @Override
     public void onEnable() {
         System.out.println(ChatColor.GREEN + "FastBoat is enabled!");
@@ -24,7 +30,7 @@ public final class Fastboat extends JavaPlugin implements Listener {
         System.out.println(ChatColor.RED + "FastBoat is disabled!");
     }
     @EventHandler
-    public void OnVehicleDrive(VehicleMoveEvent event) {
+    public void onVehicleDrive(VehicleMoveEvent event) {
         Entity vehicle = event.getVehicle();
         Entity passenger = (Entity) event.getVehicle().getPassengers();
 
@@ -32,9 +38,13 @@ public final class Fastboat extends JavaPlugin implements Listener {
             if (passenger instanceof Player) {
                 Player p = (Player) passenger;
                 Boat boat = (Boat) vehicle;
-                boat.setVelocity(new Vector(boat.getLocation().getDirection().multiply(1.5).getX(), 0, boat.getLocation().getDirection().multiply(1.5).getZ()));
+                if (p.hasPermission("fastboat.use")) {
+                    boat.setVelocity(new Vector(boat.getLocation().getDirection().multiply(1.5).getX(), 0, boat.getLocation().getDirection().multiply(1.5).getZ()));
+                } else {
+                    p.sendMessage("You do not have permission to use the boat fast, but you can still you the boat!");
+                }
+
             }
         }
     }
-
 }
