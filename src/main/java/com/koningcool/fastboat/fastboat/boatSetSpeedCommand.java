ff92a375b2c94880ff7e45a3501b4ccf1f7ffa03
koningcool <koningcool@outlook.com>
2022-06-26 09:16:20 +0200

    added permissions and aliases

--- src/main/java/com/koningcool/fastboat/fastboat/boatSetSpeedCommand.java
+++ src/main/java/com/koningcool/fastboat/fastboat/boatSetSpeedCommand.java
@@ -10,22 +10,26 @@ public class boatSetSpeedCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-
-            if(args.length == 0){
-                p.sendMessage("You did not provide any arguments while running this command. Please try again.");
-            }else{
-                if(args.length == 1){
-                    String playerissuedspeed = args[0];
-                    Fastboat.plugin.getConfig().set("speedmultiplier", playerissuedspeed);
-                    p.sendMessage(ChatColor.GREEN + "The speed has changed to" + playerissuedspeed);
+            if (p.hasPermission("fastboat.setspeed")) {
+                if (args.length == 0) {
+                    p.sendMessage("You did not provide any arguments while running this command. Please try again.");
+                } else {
+                    if (args.length == 1) {
+                        String playerissuedspeed = args[0];
+                        Fastboat.plugin.getConfig().set("speedmultiplier", playerissuedspeed);
+                        p.sendMessage(ChatColor.GREEN + "The speed has changed to" + playerissuedspeed);
+                    }
+                }
+            }else {
+                if (sender instanceof ConsoleCommandSender)
+                    System.out.println(ChatColor.RED + "Your not a player!");
+                if (sender instanceof BlockCommandSender)
+                    System.out.println("Your not a player!");
+                if (!p.hasPermission("fastboat.setspeed")){
+                    p.sendMessage("You do not have permission to set the boat speed!");
                 }
             }
-        }
-        else{
-            if(sender instanceof ConsoleCommandSender)
-                System.out.println(ChatColor.RED + "Your not a player!");
-            if(sender instanceof BlockCommandSender)
-                System.out.println("Your not a player!");
+            return false;
         }
         return false;
     }
